9a17862d885aae13e620f2c69af549a6ab18aaf5
Vitaly Tonkacheyev <thetvg@gmail.com>
2012-12-26 09:19:05 +0200

    Some fixes in tools.cpp

--- tools.cpp
+++ tools.cpp
@@ -1,7 +1,6 @@
 #include "tools.h"
 #include "glibmm/fileutils.h"
 #include <fstream>
-#include <list>
 #include <cstdlib>
 #include <vector>
 #include <map>
@@ -15,7 +14,7 @@ Glib::ustring Tools::getResPath(const char *resName)
 {
 	const Glib::ustring pathSuffix("/share/alsavolume/");
 	const Glib::ustring resName_(resName);
-	std::list<Glib::ustring> list;
+	std::vector<Glib::ustring> list;
 	Glib::ustring homepath(Glib::ustring(getenv("HOME")) + "/.local" + pathSuffix);
 	size_t cwdSize = 255;
 	char cwdBuffer[cwdSize];
@@ -24,12 +23,9 @@ Glib::ustring Tools::getResPath(const char *resName)
 	list.push_back(cwd + "/" + resName_);
 	list.push_back("/usr"+ pathSuffix + resName_);
 	list.push_back("/usr/local" + pathSuffix + resName_);
-	std::list<Glib::ustring>::iterator i = list.begin();
-	for (uint j=0; j < list.size(); j++) {
-		std::advance(i,j);
-		const char *fileName = (char*)i->c_str();
-		if (checkFileExists(fileName)) {
-			return Glib::ustring(*i);
+	for (uint i = 0; i < list.size(); i++) {
+		if (checkFileExists(list.at(i))) {
+			return Glib::ustring(list.at(i));
 		}
 	}
 	return "";
